1b103d82c5b6a70f52bb82f551b4fca6a657bc80
Abu Taher Siddik <138387257+imabutahersiddik@users.noreply.github.com>
2024-08-11 15:23:52 +0600

    Create postTweet.js

---
+++ postTweet.js
@@ -0,0 +1,29 @@
+const Twitter = require('twitter-lite');
+
+// Twitter API credentials
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+// Function to post a tweet
+const postTweet = async (tweet) => {
+  try {
+    await client.post("statuses/update", { status: tweet });
+    console.log("Tweet posted successfully!");
+  } catch (error) {
+    console.error("Error posting tweet:", error);
+  }
+};
+
+// Get the remaining years and days from environment variables
+const remainingYears = process.env.REMAINING_YEARS;
+const remainingDays = process.env.REMAINING_DAYS;
+
+// Create the tweet message
+const tweetMessage = `🚨 Another day passed! There are ${remainingYears} years and ${remainingDays} days remaining until the end of Ummath 2080. #Ummath #Countdown #MuslimCommunity #Faith`;
+
+// Post the tweet
+postTweet(tweetMessage);
